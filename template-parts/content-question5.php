a6466f79b14b2061e769d3f19ee3a95767185701
apieschel <apieschel@gmail.com>
2019-07-26 12:41:51 -0500

    explanation revision

--- template-parts/content-question5.php
+++ template-parts/content-question5.php
@@ -38,14 +38,14 @@
 	
 	<h2><?php esc_html_e('Explanation', 'apieschel'); ?></h2>
 	<p>
-		<?php echo esc_html('An anti-pattern is a common solution to a problem that is nonetheless ineffective and counterproductive. The switch_to_blog() function does not switch autoload options, plugins, or the object cache. Therefore, if any options are autoloaded, they might not be properly deleted. According to this WordPress tracking issue (https://core.trac.wordpress.org/ticket/14992), when you use switch_to_blog with an Object caching plugin installed you get incorrect links from home_url and therefore also get_permalink. In addition, in a multisite install, looping through all blogs to delete options can be very resource intensive (https://developer.wordpress.org/plugins/plugin-basics/uninstall-methods/). A shorter, more readable, and less resource intensive version of this code would simply accompany each instance of delete_option("option_name") with a call of delete_site_option("option_name"), which removes an option by name from the current network.'); ?>
+		<?php echo esc_html('An anti-pattern is a common solution to a problem that is nonetheless ineffective and counterproductive, or perhaps unnecessarily resource intensive. This code would probably qualify as an anti-pattern in the case of most WordPress multisite installations that managed a large number of sites. In a multisite install, looping through all blogs to delete options can be very resource intensive (https://developer.wordpress.org/plugins/plugin-basics/uninstall-methods/). A shorter, more readable, and less resource intensive version of this code would simply accompany each instance of delete_option(&#39;option_name&#39;) with a call of delete_site_option(&#39;option_name&#39;), which removes an option by name from the current network.'); ?>
 	</p>
 	
 	<h2><?php esc_html_e('Answer', 'apieschel'); ?></h2>
 	<div class="code">
 		<code>
 			<?php echo esc_html('<?php'); ?><br>
-			<?php echo esc_html('//file: uninstall.php'); ?><br>
+			<?php echo esc_html('// file: uninstall.php'); ?><br>
 			<?php echo esc_html('delete_option(&#39;option_name&#39;);'); ?><br>
 			<?php echo esc_html('delete_site_option(&#39;option_name&#39;);'); ?><br>
 		</code>
@@ -54,10 +54,6 @@
 	<h2><?php esc_html_e('Code Execution', 'test'); ?></h2>
 	
 	<?php
-	
-	global $wpdb;
-	$blogs = $wpdb;
-	var_dump($blogs);
 	
 	?>
 </article>
