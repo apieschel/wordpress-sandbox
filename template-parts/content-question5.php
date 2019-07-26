75aaaa407f51e6c8bd93eaba3031e8f6c2ed858f
apieschel <apieschel@gmail.com>
2019-07-25 23:44:41 -0500

    setup question 5

--- template-parts/content-question5.php
+++ template-parts/content-question5.php
@@ -18,10 +18,10 @@
 	<div class="code">
 		<code>
 			<?php echo esc_html('<?php'); ?><br>
-			<?php echo esc_html('// Assume user privileges and nonce are correctly checked'); ?><br>
-			<?php echo esc_html('$options = array();'); ?><br>
-			<?php echo esc_html('$options[&#39;option1&#39;] = isset($_POST[&#39;option1&#39;]) ? $_POST[&#39;option1&#39;] : ";'); ?><br>
-			<?php echo esc_html('update_option(&#39;my_option_key&#39;, $options);'); ?><br>
+			<?php echo esc_html('// file: uninstall.php'); ?><br>
+			<?php echo esc_html('if( is_multisite() ) {'); ?><br>
+			<?php echo esc_html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;global $wpdb;'); ?><br>
+			<?php echo esc_html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$blogs = $wpdb->get_results("SELECT blog_id FROM'); ?><br>
 		</code>
 	</div><!--.code-->
 	
@@ -33,36 +33,13 @@
 	<h2><?php esc_html_e('Answer', 'apieschel'); ?></h2>
 	<div class="code">
 		<code>
-			<?php echo esc_html('<?php'); ?><br>
-			<?php echo esc_html('// Assume user privileges and nonce are correctly checked'); ?><br>
-			<?php echo esc_html('$options = array();'); ?><br>
-			<?php echo esc_html('$options[&#39;option1&#39;] = isset($_POST[&#39;option1&#39;]) ? sanitize_text_field( wp_unslash( $_POST[&#39;option1&#39;] ) ) : "";'); ?><br><br>
-			<?php echo esc_html('wp_cache_delete ( &#39;alloptions&#39;, &#39;options&#39; );'); ?><br><br>
-			<?php echo esc_html('if( get_option(&#39;my_option_key&#39;) !== false ) {'); ?><br>
-			<?php echo esc_html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;update_option(&#39;my_option_key&#39;, $options);'); ?><br>
-			<?php echo esc_html('} else {'); ?><br>
-			<?php echo esc_html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;add_option(&#39;my_option_key&#39;, $options, null, &#39;no&#39;);'); ?><br>
-			<?php echo esc_html('}'); ?><br>
+			
 		</code>
 	</div><!--.code-->
 	
 	<h2><?php esc_html_e('Code Execution', 'test'); ?></h2>
 	
 	<?php
-		$options = array();
-		$options['option1'] = isset($_POST['option1']) ? sanitize_text_field( wp_unslash($_POST['option1']) ) : "";
 	
-		wp_cache_delete ( 'alloptions', 'options' );
-
-		if( get_option('my_option_key') !== false ) {
-			update_option('my_option_key', $options);
-			esc_html_e('This option does indeed already exist, and we have updated it. ', 'test');
-		} else {
-			esc_html_e('The option doesn\'t exist, so we\'ll add it, and tell Wordpress not to autoload it. ', 'test');
-			add_option('my_option_key', $options, null, 'no');
-		}
-		
-		esc_html_e('Here is the options array that was added to the database: ', 'test');
-		print_r($options); 
 	?>
 </article>
